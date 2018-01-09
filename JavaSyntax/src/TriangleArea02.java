3ffed681a31bd2d04ecd155df77627db347a2ad4
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-01-09 13:26:16 +0200

    Update TriangleArea02.java

--- JavaSyntax/src/TriangleArea02.java
+++ JavaSyntax/src/TriangleArea02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TriangleArea02 {
+public class TriangleArea02 {//100/100
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
