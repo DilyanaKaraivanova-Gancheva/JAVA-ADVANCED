ec844ed7aa2d89d586fa7413e95c70ce5d657417
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-01-09 13:27:11 +0200

    Update RectangleArea01.java

--- JavaSyntax/src/RectangleArea01.java
+++ JavaSyntax/src/RectangleArea01.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class RectangleArea01 {
+public class RectangleArea01 {// 100/100
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         Double a = scanner.nextDouble();
